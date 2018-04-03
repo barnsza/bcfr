7db021f2ec3766a9b0e5df57b4ad8578bb3893bf
Richard Barnett <barnsza@users.noreply.github.com>
2018-04-03 21:27:12 +0200

    Added ability to negate (!) / whitelist entries in gravity sources.

--- BuildGravity.py
+++ BuildGravity.py
@@ -7,8 +7,17 @@ gravity = kyotocabinet.DB()
 gravity.open('gravity.kch', kyotocabinet.DB.OWRITER |
     kyotocabinet.DB.OCREATE | kyotocabinet.DB.OTRUNCATE)
 
+whitelist = kyotocabinet.DB()
+whitelist.open(':')
+
 source = 'https://raw.githubusercontent.com/barnsza/BlockingCloudFlareResolver/master/sources.txt'
 seen_urls = set()
+
+def process_record(record):
+    if record[0] == '!':
+        whitelist['{0}.'.format(record[1:])] = None
+    else:
+        gravity['{0}.'.format(record)] = None
 
 def process_url(url):
     if url in seen_urls:
@@ -30,11 +39,12 @@ def process_url(url):
             if re.search('https?://', record[0]):
                 process_url(record[0])
             else:
-                gravity['{0}.'.format(record[0])] = None
+                process_record(record[0])
         else:
-            gravity['{0}.'.format(record[1])] = None
+            process_record(record[1])
 
 process_url(source)
+whitelist.iterate(lambda k, v: gravity.remove(k))
 print('Stats: {0} domains, {1} bytes.'.format(gravity.count(), gravity.size()))
 
 gravity.close()
